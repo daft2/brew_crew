120d1483fedd287d11af044da405ceaeeb22f52d
Daft <flushid@gmail.com>
2021-02-07 23:00:55 +0700

    add page and service for auth & function login anonymously

---
+++ lib/screens/authenticate/sign_in.dart
@@ -0,0 +1,38 @@
+import 'package:brew_crew/services/auth.dart';
+import 'package:flutter/material.dart';
+
+class SignIn extends StatefulWidget {
+  @override
+  _SignInState createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  final AuthService _auth = AuthService();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.brown[100],
+      appBar: AppBar(
+        backgroundColor: Colors.brown[400],
+        elevation: 0.0,
+        title: Text('Sign In'),
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
+        child: RaisedButton(
+          child: Text('Sign In Anonymous'),
+          onPressed: () async {
+            dynamic data = await _auth.signInAnon();
+            if (data == null) {
+              print('Error Sign In');
+            } else {
+              print('Sucessfully Signed In');
+              print(data);
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
